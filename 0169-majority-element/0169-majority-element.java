cae815f3ebef33cbb2f26552e3e57da9ca9d297d
MONIKA ANIL GHADGE <monikaghadage25@gmail.com>
2025-06-17 09:33:03 +0530

    Time: 7 ms (38.27%), Space: 53.3 MB (30.28%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+    if(nums.length==1)
+    {
+       return nums[0];
+    }
+    else{
+    int count=0;
+    for(int i=0;i<nums.length-1;i++)
+    {
+       if(nums[i]==nums[i+1])
+       {
+         count++;
+         if(count>=nums.length/2)
+         {
+           return nums[i];
+         }
+       }
+       else
+       {
+          count=0;
+       }
+    }}
+    return 0;
+    }
+}
